ff2f3254ec86a6f12c754dc46048c175922226b1
Xia Zhao <xiazhao@xiadeair.lan>
2021-08-21 20:45:34 -0700

    [copy-from-usb][phase2] Update FileUtils.getNonLocalDiskPath method to handle mac OS system.

--- copy-from-usb/src/main/java/com/playground/utils/FileUtils.java
+++ copy-from-usb/src/main/java/com/playground/utils/FileUtils.java
@@ -13,6 +13,29 @@ public class FileUtils {
     private static final String LOCAL_DISK = "Local Disk";
 
     public static List<File> getNonLocalDiskPath() {
+        if (isMacOs()) { // for Mac OS
+            return getMacOsNonLocalDiskPath();
+        } else { // for Windows
+            return getWindowsNonLocalDiskPath();
+        }
+    }
+
+    private static boolean isMacOs() {
+        return System.getProperty("os.name").startsWith("Mac OS");
+    }
+
+    private static List<File> getMacOsNonLocalDiskPath() {
+        final List<File> nonLocalDisks = new ArrayList<>();
+        final FileSystemView fsv = FileSystemView.getFileSystemView();
+        for (File file: new File("/Volumes").listFiles()) {
+            if (file.canRead() && file.canWrite() && file.getName().endsWith("USB")) {
+                nonLocalDisks.add(file);
+            }
+        }
+        return nonLocalDisks;
+    }
+
+    private static List<File> getWindowsNonLocalDiskPath() {
         final List<File> nonLocalDisks = new ArrayList<>();
         final FileSystemView fsv = FileSystemView.getFileSystemView();
         for (File file: File.listRoots()) {
