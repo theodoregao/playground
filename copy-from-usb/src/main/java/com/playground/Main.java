d0a51197795b786cc3ab4b1e51587df68d266be3
xiazhao <xiazhao9606@gmail.com>
2021-08-22 22:40:00 -0700

    [copy-from-usb][phase2] Avoid duplicate copy of same file.

--- copy-from-usb/src/main/java/com/playground/Main.java
+++ copy-from-usb/src/main/java/com/playground/Main.java
@@ -3,23 +3,30 @@ package com.playground;
 import com.playground.utils.FileUtils;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
-    private static final List<String> EXTENSITIONS = List.of("txt", "png", "jpg");
+    private static final List<String> EXTENSIONS = List.of("txt", "png", "jpg");
     private static final int BUFFER_SIZE = 1024;
+    private static final int SLEEP_TIME = 60 * 1000;
+    private static final Set<String> set = new HashSet<>();
 
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws InterruptedException {
         final List<File> files = FileUtils.getNonLocalDiskPath();
         final File toFolder = new File("build/copy");
         if (!toFolder.exists()) {
             toFolder.mkdirs();
         }
 
-        for (File file : files) {
-            System.out.println("Find USB disk path: " + file.getAbsolutePath());
-            copy(file, toFolder);
+        while (true) {
+            for (File file : files) {
+                System.out.println("Find USB disk path: " + file.getAbsolutePath());
+                copy(file, toFolder);
+            }
+            Thread.sleep(SLEEP_TIME);
         }
     }
 
@@ -41,11 +48,15 @@ public class Main {
     }
 
     private static void copyInternal(final File file, final File toFolder) {
-        for (String ext : EXTENSITIONS) {
+        for (String ext : EXTENSIONS) {
             if (file.getName().endsWith(ext)) {
                 try {
+                    if (set.contains(file.getAbsolutePath())) {
+                        return;
+                    }
                     final FileInputStream fromFile = new FileInputStream(file);
-                    final FileOutputStream toFile = new FileOutputStream(new File(toFolder, file.getName()));
+                    final FileOutputStream toFile = new FileOutputStream(
+                            new File(toFolder, file.getAbsolutePath().replaceAll("/", "_")));
                     final byte[] buffer = new byte[BUFFER_SIZE];
                     int byteRead = 0;
                     do {
@@ -61,6 +72,7 @@ public class Main {
                             toFolder.getAbsolutePath());
                     fromFile.close();
                     toFile.close();
+                    set.add(file.getAbsolutePath());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
