1baedf6363b4651154027024da6a048ff067c5a1
xiazhao <xiazhao9606@gmail.com>
2021-08-30 23:12:40 -0700

    [setup] Update gitignore and gradle files.

--- server/copy-from-usb/build.gradle.kts
+++ server/copy-from-usb/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     java
 }
@@ -11,4 +13,23 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Implementation-Title"] = "Copy from USB"
+        attributes["Implementation-Version"] = "1.0"
+        attributes["Main-Class"] = "com.playground.Main"
+    }
+    from(configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
