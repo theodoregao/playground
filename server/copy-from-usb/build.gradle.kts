939a74b19d80120445cde49a28b5b1277d8b5488
theodoregao <theodore.gao.1986@gmail.com>
2021-08-30 21:29:17 -0700

    [copy-from-usb] Add gson dependency.

--- server/copy-from-usb/build.gradle.kts
+++ server/copy-from-usb/build.gradle.kts
@@ -7,6 +7,7 @@ tasks.withType<Test> {
 }
 
 dependencies {
+    implementation("com.google.code.gson:gson:2.8.8")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
